842b1b405805ad43eea35b9ba0c279b4eb1046c1
jdbogieman <justin.diblasi@gmail.com>
2016-01-12 01:03:12 -0600

    arrays for lemonade stand

---
+++ lemonadestand.js
@@ -0,0 +1,12 @@
+
+var week;
+
+week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var weatherTypes;
+
+weatherTypes = ["Rain", "Sun", "Thunderstorm", "Tornado", "Hurricane", "Hot", "Cool"];
+
+
+
+dailyTemp = ["65", "70", "75", "80", "85", "90", "95", "100"];
